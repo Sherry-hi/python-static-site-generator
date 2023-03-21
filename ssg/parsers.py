b01433cf30bd9777fb6e4869946516cfcac88e2b
Sherry Fu <shuang.fu@sas.com>
2023-03-21 10:59:05 +0800

    module2
    module2

---
+++ ssg/parsers.py
@@ -0,0 +1,32 @@
+from typing import List
+from pathlib import Path
+import shutil
+
+class Parser:
+    def __init__(self,extensions):
+        self.extensions=[]
+    def valid_extension(self, extension):
+        return extension in self.extensions
+             
+        
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
+    
+    def read(self, path):
+        with open(path,"r") as file:
+            return file.read()
+            
+    def write(self, path, dest, content, ext=".html"):
+        full_path= dest / path.with_suffix.name
+        with open(path,"w") as file:
+            file.write(content)
+            
+    def copy(self, path, source, dest):
+        shutil.copy2(path, dest / path.relative_to(source))
+        
+class ResourceParser (Parser):
+    extensions = [".jpg", "png", "gif", ".css", ".html"]
+    def parse(self, path, source, dest):
+        self.copy(path, source, dest)
+        
+        
